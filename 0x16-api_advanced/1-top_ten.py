399fb9324812a5edbe0b27f7fe2aabdd85e5817a
deibyocampo <deiby13_96@hotmail.com>
2020-02-07 00:12:04 -0500

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+module for a funtion
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    returns the numbers of subscribers
+    """
+    url = 'http://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    user_agent = {'User-Agent': 'together'}
+    req = requests.get(url, headers=user_agent, allow_redirects=False,
+                       params={'limit': 10})
+    if req.status_code == 200:
+        req = req.json()
+        data = req.get('data')
+        children = data.get('children')
+        if data is not None and children is not None:
+            for post in children:
+                post_data = post.get('data')
+                title = post_data.get('title')
+                print(title)
+    else:
+        print('None')
