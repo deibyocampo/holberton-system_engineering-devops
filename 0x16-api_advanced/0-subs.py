99d8cc9e1d63df54b48d51fa306480d5808d9727
deibyocampo <deiby13_96@hotmail.com>
2020-02-09 23:42:40 -0500

    change user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,8 @@ def number_of_subscribers(subreddit):
     """
     returns the numbers of subscribers
     """
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    user_agent = {'User-Agent': 'together'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    user_agent = {'User-Agent': 'Chrome/2602:301:772d:c490:ec20:8e49:3647:8db6'}
     req = requests.get(url, headers=user_agent, allow_redirects=False)
     if req.status_code == 200:
         req = req.json()
