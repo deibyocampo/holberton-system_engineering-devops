c70ff2a2dee2d0082ab445a3450063c25bd78dbd
deibyocampo <deiby13_96@hotmail.com>
2020-02-09 23:48:51 -0500

    pep8 limit characters

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-module for a funtion
+module for a function a subscribers
 """
 import requests
 
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     returns the numbers of subscribers
     """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    user_agent = {'User-Agent': 'Chrome/2602:301:772d:c490:ec20:8e49:3647:8db6'}
+    user_agent = {'User-Agent': 'Derek@holberton'}
     req = requests.get(url, headers=user_agent, allow_redirects=False)
     if req.status_code == 200:
         req = req.json()
