2d2cf6ecd06fb8f07fdc03d7ef74da5fdc8dd4ec
deibyocampo <deiby13_96@hotmail.com>
2020-02-22 18:48:02 -0500

    puppet custom http

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 #puppet manifest to install a nginx server
 
-exec {'Install nginx':
-  command => 'sudo apt -y update && sudo apt -y install nginx && sudo sed -i
-  "15i \ \tadd_header X-Served-By \$hostname;", /etc/nginx/nginx.conf && sudo service nginx restart',
-    provider => shell,
-    }
+exec {'install nginx':
+  command => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo sed -i "15i aadd_header X-Served-By \$hostname;" /etc/nginx/nginx.conf && sudo service nginx restart',
+  provider => shell,
+  }
