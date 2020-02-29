a19a519be129f96627d697b15144fc0d1fd7058f
deibyocampo <deiby13_96@hotmail.com>
2020-02-29 17:45:37 -0500

    change the configutation

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 #Change the OS configuration
 exec { 'User Limit':
-  command => 'sed -i \'s/nofile 5/nofile 3000/; s/nofile 4/nofile 2000/\' /etc/segurity/limits.conf',
+  command => 'sed -i \'s/nofile 5/nofile 3000/; s/nofile 4/nofile 2000/\' /etc/security/limits.conf',
   provider => shell,
   }
